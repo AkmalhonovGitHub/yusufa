3371b126eadbf03679e3ca79aeeaf875933fbecb
akramovasadbek <fifaandothergames01@gmail.com>
2023-02-25 11:55:59 +0500

    ff

---
+++ src/Asad/Asad.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Asad.css";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+function Asad(props) {
+    return (
+        <div>
+            <section className="asad-1">
+                <div className="container container-fluid">
+                    <h1 className="text-danger">OUR WORK</h1>
+                </div>
+            </section>
+        </div>
+    )
+}
